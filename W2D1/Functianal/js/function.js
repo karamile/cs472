65c048a7e3c4fcbf0871f60141b0ac7635663afa
karamile <kapello2013@gmail.com>
2020-07-28 00:25:09 -0500

    W2D1  Commit

--- W2D1/Functianal/js/function.js
+++ W2D1/Functianal/js/function.js
@@ -1,19 +1,21 @@
 /* 4 Define a function sum() and a function multiply() 
 that sums and multiplies (respectively) all the numbers in an array of numbers */
 /* sum */
-const sum = arr.reduce(function (accumulator, currentValue) {
+const array1 = [1, 2, 3, 4];
+let initialValue = 0
+const sum = array1.reduce(function (accumulator, currentValue) {
     return accumulator + currentValue
-  }, 0)
+  }, initialValue)
 /* Alternatively written with an arrow function: */
-const total = arr.reduce(( accumulator, currentValue ) => accumulator + currentValue,0)
+const total = array1.reduce(( accumulator, currentValue ) => accumulator + currentValue,0)
 
-/*m ultiply */
-const multiply=arr.reduce(function(accumulator,currentValue){
+/*multiply */
+const multiply=array1.reduce(function(accumulator,currentValue){
     return accumulator * currentValue
 },1)
 /* Alternatively written with an arrow function: */
- const product=arr.reduce((accumulator,currentValue)=>accumulator * currentValue,1)
-/******************************************************************************************************* */
+ const product=array1.reduce((accumulator,currentValue)=>accumulator * currentValue,1)
+
  /*  5 Define a function reverse() that computes the reversal of a string. */
  /* one way */
  String.prototype.reverse = function(){
@@ -24,17 +26,19 @@ var reverse= function(str){
     return str.split('').reverse().join('');
 }
 /* third  way */
-String.split('').reverse().join('');
-/******************************************************************************************************* */
+//String.split('').reverse().join('');
+
 /* 7 Write a function filterLongWords() 
 that takes an array of words and an integer i and returns the array of words that are longer than i. */ 
-function filterLongWords( string){
-    return string.length > i;
-}
-var words = strings.filter(filterLongWords);
+ let words = ['Alice', 'Bob', 'Tiff', 'Bruce', 'Alice']
+
+// function filterLongWords( string){
+//     return string.length > i;
+// }
+// var words = strings.filter(filterLongWords);
  /* second way */
-let filterLongWords = words.filter(word => {
-    return word.length > i;
-})
+// let filterLongWords2 = words.filter(word => {
+//     return word.length > i;
+// })
  /* third  way */
-const filterLongWords = (words, length) => words.filter(str => str.length > length);
+const filterLongWords1 = (words, length) => words.filter(str => str.length > length);
